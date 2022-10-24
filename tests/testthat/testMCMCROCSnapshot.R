15e98eaa367cac41e16ca22df4171af34d86f99d
Elizabeth Barnes <ebarne24@vols.utk.edu>
2022-10-24 09:12:50 -0400

    committed in order to create new brach and connect to remote repository

--- tests/testthat/testMCMCROCSnapshot.R
+++ tests/testthat/testMCMCROCSnapshot.R
@@ -1,8 +1,7 @@
-library(AnaCoDa)
 
+#loading libraries
+library(AnaCoDa)
 library(testthat)
-local_edition(3)
-Config/testthat/edition: 3
 
 rm(list=ls(all.names=TRUE))
 context("MCMC with ROC")
@@ -24,52 +23,73 @@ thinning <- 10
 adaptiveWidth <- 10
 divergence.iteration <- 0
 
-#run MCMC - with phi
-mcmc <- initializeMCMCObject(samples = samples, thinning = thinning, adaptive.width = adaptiveWidth, 
-                             est.expression=TRUE, est.csp=TRUE, est.hyper=TRUE)
-
 set.seed(446141)
 
-genome <- initializeGenomeObject(file = fileName, observed.expression.file = expressionFile, match.expression.by.id=FALSE)
+#creating data frame containing possibilities for estimating or fixing phi, dM, and dE
+##true or false factor is created simply to allow me to replicate the list more easily
+T_or_F_factor <- c(TRUE,FALSE)
+T_or_F <- data.frame(est_phi = rep(T_or_F_factor, each = 4),
+                     est_dM = c(rep(T, 2), rep(F,2)),
+                     est_dE = c(T,F))
 
-geneAssignment <- sample(c(1,2), size = length(genome), replace = TRUE, prob = c(0.3, 0.7)) #c(rep(1,500), rep(2,500))
+##objects containing true and false possibilities from columns of T_or_F data frame
+est_phi <- T_or_F$est_phi
+est_dM <- T_or_F$est_dM
+est_dE <- T_or_F$est_dE
 
-parameter <- initializeParameterObject(genome, sphi_init, numMixtures, geneAssignment, split.serine = TRUE, mixture.definition = mixDef)
-parameter$initSelectionCategories(c(selectionMainFile, selectionHtFile), 2,F)
-parameter$initMutationCategories(c(mutationMainFile, mutationHtFile), 2,F)
+#for loop begins
 
-model <- initializeModelObject(parameter, "ROC", with.phi = TRUE) 
-
-outFile <- file.path("UnitTestingOut", "testMCMCROCLogPhi.txt")
-
-sink(outFile)
-runMCMC(mcmc, genome, model, 1, divergence.iteration)
-sink()
-
-print(mcmc$getLogLikelihoodTrace()[10])
-#known logLik for est_phi <- -2250948
-
-#2022-09-09: begin snapshot testing by Elizabeth Barnes
-T_or_F <- data.frame(est_phi = c("T", "T", "T", "F", "F", "F"),
-                 est_dM = c("T", "F", "T", "T", "F", "F"),
-                 est_dE = c("T", "F", "F", "T", "T","F"),
-                 logLik = c(-2250948, -2250948, -2250948, "NULL", "NULL", "NULL"))
-
-
-test_cond <- c("est_phi", "est_dM", "est_dE")
-test_val <- c("logLik", "vardM", "vardE")
-test_msg <- paste0("Test cond: est_phi", "Test value: logLik", print(mcmc$getLogLikelihoodTrace()[10]))
-
-for(est_phi in T_or_F) {
-  test_cond <- T_or_F$est_phi=="T"
-
-test_that(test_msg, {
-  local_edition(3)
-  expect_snapshot(T_or_F$est_phi[4], mcmc$getLogLikelihoodTrace()[10])
-})
+for(est_phi) {
+  for(est_dM) {
+    for(est_dE) {
+        
+      genome <- initializeGenomeObject(file = fileName, observed.expression.file = expressionFile, match.expression.by.id=FALSE)
       
-
+      geneAssignment <- sample(c(1,2), size = length(genome), replace = TRUE, prob = c(0.3, 0.7)) #c(rep(1,500), rep(2,500))
+      
+      parameter <- initializeParameterObject(genome, sphi_init, numMixtures, geneAssignment, split.serine = TRUE, mixture.definition = mixDef)
+      parameter$initSelectionCategories(c(selectionMainFile, selectionHtFile), 2,F)
+      parameter$initMutationCategories(c(mutationMainFile, mutationHtFile), 2,F)
+      
+      model <- initializeModelObject(parameter, "ROC", with.phi = est_phi) 
+      
+      outFile <- file.path("UnitTestingOut", "testMCMCROCLogPhi.txt")
+      
+      sink(outFile)
+      runMCMC(mcmc, genome, model, 1, divergence.iteration)
+      sink()
+      
+      #get log likelihood trace when 'with phi' is true for tenth sample
+      actual_loglik <- mcmc$getLogLikelihoodTrace()[samples])
+      
+###testing begins for log likelihood
+      test_that("get expected log likelihood value when 'with phi' is true, not expected log likelihood when 'with phi' is false", {
+        parameter <- initializeParameterObject(genome, sphi_init, numMixtures, geneAssignment, split.serine = TRUE, mixture.definition = mixDef)
+        parameter$initSelectionCategories(c(selectionMainFile, selectionHtFile), 2,F)
+        parameter$initMutationCategories(c(mutationMainFile, mutationHtFile), 2,F)
+        
+        model <- initializeModelObject(parameter, "ROC", with.phi = TRUE) 
+        
+        outFile <- file.path("UnitTestingOut", "testMCMCROCLogPhi.txt")
+        
+        sink(outFile)
+        runMCMC(mcmc, genome, model, 1, divergence.iteration)
+        sink()
+        
+        expect_loglik <- mcmc$getLogLikelihoodTrace([samples])
+        expect_value(expect_loglik, actual_loglik )
+      }
+      
+      
+      
+    }
+  }
 }
+
+
+
+
+
 
 
              
